6e4eed3161b480cb3cb387938baf67d5ec4bf953
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-07-26 21:15:41 +0800

    changed Config.py

--- config/Config.py
+++ config/Config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-    config_dir = os.path.join(os.getcwd(), "config")
+    config_dir = os.path.dirname(os.path.realpath(__file__))
     if not("super.config" in os.listdir(config_dir)):
         os.symlink(f"{config_dir}/secret.config", f"{config_dir}/super.config")
-    os.path.dirname(os.path.realpath(__file__))
+
     with open(f"{config_dir}/super.config", "r") as f:
         config = json.loads(f.read())
 
