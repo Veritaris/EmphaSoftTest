4bba1d3bc7394d0c668044cf5ab3f4eefb9d5759
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-07-26 20:34:35 +0800

    made some changes, bla bla bla

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,19 @@
-from flask import jsonify
-
+from flask import request
 from app import app
 from app import API
 
 
 @app.route("/", methods=["GET"])
 def render_main():
+    # if request.cookies.get("user_logged")
     return API.main_page()
+
 
 @app.route("/login", methods=["POST"])
 def login_to_vk():
     return API.login_to_vk()
+
+
+@app.route("/login", methods=["GET"])
+def is_logged():
+    return API.is_logged()
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+cd ~/$1
+git pull --force
+
+if ! cmp requirements.txt requirements.super.txt >/dev/null 2>&1
+then
+  echo -e "\e[93mDetected changes in requirements, reinstalling packages..."
+  echo -e "\e[93mCopying requirements.txt to requirements.super.txt..."
+  cp requirements.txt requirements.super.txt
+  echo -e "\e[92mDone"
+  echo -e "\e[93mInstalling packages..."
+  if pip install -r requirements.txt
+  then
+    echo "\e[92Done."
+  fi
+fi
+
+sudo service unit reload
+sudo service nginx reload
