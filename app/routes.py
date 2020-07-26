065ef86c09a413f9936af8f1713c3b0cddd74b36
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-07-26 21:17:09 +0800

    changed Config.py

--- app/routes.py
+++ app/routes.py
@@ -3,17 +3,17 @@ from app import app
 from app import API
 
 
-@app.route("/", methods=["GET"])
+@app.route("/est", methods=["GET"])
 def render_main():
     # if request.cookies.get("user_logged")
     return API.main_page()
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/est/login", methods=["POST"])
 def login_to_vk():
     return API.login_to_vk()
 
 
-@app.route("/login", methods=["GET"])
+@app.route("/est/login", methods=["GET"])
 def is_logged():
     return API.is_logged()
