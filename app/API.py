d2bce7392328e2a667449714528419b994129412
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-07-27 15:17:23 +0800

    made get friends

--- app/API.py
+++ app/API.py
@@ -28,12 +28,12 @@ for i in range(5):
 
 handler = RotatingFileHandler(f"{log_dir}/app.log", maxBytes=1000000, backupCount=8)
 handler.setLevel(logging.INFO)
-# handler.setFormatter(logging.Formatter(
-#     '%(hostip)s [%(asctime)s] -- %(message)s'
-# ))
 handler.setFormatter(logging.Formatter(
-    '[%(asctime)s] -- %(message)s'
+    '%(hostip)s [%(asctime)s] -- %(message)s'
 ))
+# handler.setFormatter(logging.Formatter(
+#     '[%(asctime)s] -- %(message)s'
+# ))
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -41,7 +41,7 @@ logger.addHandler(handler)
 
 token_url = "https://oauth.vk.com/access_token"
 oauth_url = "https://oauth.vk.com/authorize"
-vk_api_url = "https://api.vk.com/method/"
+vk_api_url = "https://api.vk.com/method/{}"
 
 
 def stringify_cookie(cookie: dict):
@@ -61,6 +61,7 @@ def log(func):
             extra={"hostip": request.url}
         )
         return func()
+
     return wrap
 
 
@@ -85,7 +86,7 @@ def login_to_vk():
                 "redirect_uri": "https://167.71.58.132/est/getCode",
                 "scope": 2,
                 "response_type": "code",
-                "v": 5.120
+                "v": "5.120"
             }
         )
         return r.content
@@ -111,21 +112,12 @@ def get_code():
         if r.json().get("access_token"):
             token = r.json().get("access_token")
             user = dbsession.query(Users).get(session.get("user"))
+            user.user_code = str(code)
             user.user_token = str(token)
             dbsession.commit()
-        return r.content
+        return redirect("/est")
     else:
         return jsonify(req), 200
-
-
-@log
-def get_token():
-    req = url_decode(request.url)
-
-    if "error" in req:
-        return jsonify({"error": req.get("error"), "error_description": req.get("error_description")}), 400
-
-    return request.url, 200
 
 
 @log
@@ -133,8 +125,40 @@ def show_friend():
     if not dbsession.query(Users).get(session.get("user")):
         session.pop("user", None)
         return redirect("/est")
-    # return render_template("home.html", user="testuser", friends=fake_friend), 200
-    return request.url
+
+    user_token = dbsession.quert(Users).get(session.get("user")).user_token
+    me = post(
+        url=vk_api_url.format("users.get"),
+        data={
+            "v": "5.120",
+            "access_token": user_token
+        }
+    )
+    username = " ".join([me.json().get("response")[0].get("first_name"), me.json().get("response")[0].get("last_name")])
+    friends_ids = ",".join(map(str, post(
+            url=vk_api_url.format("friends.get"),
+            data={
+                "v": "5.120",
+                "access_token": user_token,
+                "count": 5
+            }).json().get("response").get("items")
+        )
+    )
+    friends_data = post(
+        url=vk_api_url.format("users.get"),
+        data={
+            "v": "5.120",
+            "access_token": user_token,
+            "user_ids": friends_ids
+        }
+    )
+    friends = []
+    for user in friends_data.json().get("response"):
+        friends.append(
+            (user.get("first_name"), user.get("last_name"), user.get("id"))
+        )
+
+    return render_template("home.html", user=username, friends=friends), 200
 
 
 def logout():
