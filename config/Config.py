714b1a4ec531a8b0c714fd19e0ce368225b098b5
Georgii Imeshkenov <georgiiimeshkenov@gmail.com>
2020-07-27 11:36:06 +0800

    Made get token

--- config/Config.py
+++ config/Config.py
@@ -3,14 +3,18 @@ import os
 
 
 class Config(object):
-    config_dir = os.path.dirname(os.path.realpath(__file__))
-    if not("super.config" in os.listdir(config_dir)):
-        os.symlink(f"{config_dir}/secret.config", f"{config_dir}/super.config")
+    parent_dir = "/".join(os.path.realpath(__file__).split("/")[:-2])
+    if not("super.config" in os.listdir(f"{parent_dir}/config")):
+        os.symlink(f"{parent_dir}/config/secret.config", f"{parent_dir}/config/super.config")
 
-    with open(f"{config_dir}/super.config", "r") as f:
+    with open(f"{parent_dir}/config/super.config", "r") as f:
         config = json.loads(f.read())
 
     CLIENT_ID = config["client_id"]
     APP_SECRET_KEY = config["secretKey"]
+    SECRET_KEY = config["secretKey"]
     ACCESS_TOKEN = config["access_token"]
     v = config["v"]
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{parent_dir}/database/database.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
---
+++ database/DatabaseModels.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+from app import database
+
+
+def dump_datetime(value):
+    if value is None:
+        return None
+    return [value.strftime("%Y-%m-%d"), value.strftime("%H:%M:%S")]
+
+
+class Users(database.Model):
+    __tablename__ = "Users"
+    user_uuid = database.Column(database.String(64), primary_key=True)
+    user_token = database.Column(database.String(64), unique=True)
+
+    def __repr__(self):
+        return f"<User {self.user_uuid}>"
+
+    @property
+    def serialize(self):
+        return {
+            "uuid": self.user_uuid,
+            "token": self.user_token
+        }
